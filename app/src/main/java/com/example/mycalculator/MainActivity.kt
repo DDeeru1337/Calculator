bba30185a29a697ab5bbdbe1962e505ab0b58bef
DDeer <ddeeru1337@gmail.com>
2024-01-24 21:24:56 +0300

    Сделать базовый дизайн и начать логику кнопок

---
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -0,0 +1,255 @@
+package com.example.mycalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mycalculator.ui.theme.MyCalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyCalculatorTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.Black
+                ) {
+                    Column {
+                        Spacer(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(80.dp)
+                                .background(Color.Red)
+                        )
+                        Column {
+                            Text(
+                                text = "1488",
+                                fontSize = 60.sp,
+                                color = Color.White
+                            )
+                            Text(
+                                text = "1337+3228",
+                                fontSize = 60.sp,
+                                color = Color.White
+                            )
+                            Text(
+                                text = "=12345678",
+                                fontSize = 50.sp,
+                                color = Color.Gray
+                            )
+                        }
+                        Column {
+                            Row(
+                                Modifier
+                                    .fillMaxWidth()
+                                    .weight(1f)
+                            ) {
+                                Button(
+                                    onClick = {},
+                                    colors = ButtonDefaults.buttonColors(Color.DarkGray),
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .height(70.dp)
+                                ) {
+                                    Text(text = "1",
+                                        fontSize = 35.sp)
+                                }
+                                Button(
+                                    onClick = {},
+                                    colors = ButtonDefaults.buttonColors(Color.DarkGray),
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .height(70.dp)
+                                ) {
+                                    Text(text = "2",
+                                        fontSize = 35.sp)
+                                }
+                                Button(
+                                    onClick = {},
+                                    colors = ButtonDefaults.buttonColors(Color.DarkGray),
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .height(70.dp)
+                                ) {
+                                    Text(text = "3",
+                                        fontSize = 35.sp)
+                                }
+                                Button(
+                                    onClick = {},
+                                    colors = ButtonDefaults.buttonColors(Color.DarkGray),
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .height(70.dp)
+                                ) {
+                                    Text(text = "*",
+                                        fontSize = 35.sp)
+                                }
+                            }
+                            Row(
+                                Modifier
+                                    .fillMaxWidth()
+                                    .weight(1f)
+                            ) {
+                                Button(
+                                    onClick = {},
+                                    colors = ButtonDefaults.buttonColors(Color.DarkGray),
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .height(70.dp)
+                                ) {
+                                    Text(text = "4",
+                                        fontSize = 35.sp)
+                                }
+                                Button(
+                                    onClick = {},
+                                    colors = ButtonDefaults.buttonColors(Color.DarkGray),
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .height(70.dp)
+                                ) {
+                                    Text(text = "5",
+                                        fontSize = 35.sp)
+                                }
+                                Button(
+                                    onClick = {},
+                                    colors = ButtonDefaults.buttonColors(Color.DarkGray),
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .height(70.dp)
+                                ) {
+                                    Text(text = "6",
+                                        fontSize = 35.sp)
+                                }
+                                Button(
+                                    onClick = {},
+                                    colors = ButtonDefaults.buttonColors(Color.DarkGray),
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .height(70.dp)
+                                ) {
+                                    Text(text = "/",
+                                        fontSize = 35.sp)
+                                }
+                            }
+                            Row(
+                                Modifier
+                                    .fillMaxWidth()
+                                    .weight(1f)
+                            ) {
+                                Button(
+                                    onClick = {},
+                                    colors = ButtonDefaults.buttonColors(Color.DarkGray),
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .height(70.dp)
+                                ) {
+                                    Text(text = "7",
+                                        fontSize = 35.sp)
+                                }
+                                Button(
+                                    onClick = {},
+                                    colors = ButtonDefaults.buttonColors(Color.DarkGray),
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .height(70.dp)
+                                ) {
+                                    Text(text = "8",
+                                        fontSize = 35.sp)
+                                }
+                                Button(
+                                    onClick = {},
+                                    colors = ButtonDefaults.buttonColors(Color.DarkGray),
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .height(70.dp)
+                                ) {
+                                    Text(text = "9",
+                                        fontSize = 35.sp)
+                                }
+                                Button(
+                                    onClick = {},
+                                    colors = ButtonDefaults.buttonColors(Color.DarkGray),
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .height(70.dp)
+                                ) {
+                                    Text(text = "+",
+                                        fontSize = 35.sp)
+                                }
+                            }
+                            Row(
+                                Modifier
+                                    .fillMaxWidth()
+                                    .weight(1f)
+                            ) {
+                                Button(
+                                    onClick = {},
+                                    colors = ButtonDefaults.buttonColors(Color.DarkGray),
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .height(70.dp)
+                                ) {
+                                    Text(text = "<-",
+                                        fontSize = 35.sp)
+                                }
+                                Button(
+                                    onClick = {},
+                                    colors = ButtonDefaults.buttonColors(Color.DarkGray),
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .height(70.dp)
+                                ) {
+                                    Text(text = "0",
+                                        fontSize = 35.sp)
+                                }
+                                Button(
+                                    onClick = {},
+                                    colors = ButtonDefaults.buttonColors(Color.DarkGray),
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .height(70.dp)
+                                ) {
+                                    Text(text = "=",
+                                        fontSize = 35.sp)
+                                }
+                                Button(
+                                    onClick = {},
+                                    colors = ButtonDefaults.buttonColors(Color.DarkGray),
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .height(70.dp)
+                                ) {
+                                    Text(text = "-",
+                                        fontSize = 35.sp)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
