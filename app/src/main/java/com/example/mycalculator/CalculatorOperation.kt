bc90bd59072a08d8365f1ccb994caf53aacbd649
DDeer <ddeeru1337@gmail.com>
2024-01-25 18:53:47 +0300

    Создать CalculatorOperation

--- app/src/main/java/com/example/mycalculator/CalculatorOperation.kt
+++ app/src/main/java/com/example/mycalculator/CalculatorOperation.kt
@@ -1,3 +1,8 @@
 package com.example.mycalculator
 
-sealed class CalculatorOperation
+sealed class CalculatorOperation(val symbol: String) {
+    object Multiply: CalculatorOperation("*")
+    object Divide: CalculatorOperation("/")
+    object Add: CalculatorOperation("+")
+    object Subtract: CalculatorOperation("-")
+}
